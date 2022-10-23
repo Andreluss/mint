6021e9c5c9bd66579a75905796c23f6e6e2749c2
Andrzej Jabłoński <andrzoff@gmail.com>
2022-10-23 17:09:28 +0200

    Create mint class.cpp
    archive on github

---
+++ mint class.cpp
@@ -0,0 +1,266 @@
+#include <bits/stdc++.h>
+using namespace std;
+using LL = long long;
+#define SIZE(x) (int)(x).size()
+#define FOR(i, b, e) for(auto i = (b); i <= (e); i++)
+#define DFOR(i, b, e) for(auto i = (b); i >= (e); i--)
+#define REP(cnt) FOR(_iter, 0, (cnt-1))
+#define P2(x) (1LL<<(x))
+#define st first
+#define nd second
+#define mt make_tuple
+#define SONIC_SPEED() ios::sync_with_stdio(0); cin.tie(0);
+#define ALL(x) (x).begin(), (x).end()
+
+constexpr LL INF = LLONG_MAX / 4;//max 4 razy w sumie
+constexpr int iINF = INT_MAX / 2 - 1;
+
+#define DEBUG 0
+#if DEBUG
+	#include <ultimate_power.h>
+#else 
+	#define echo(...)
+	//#undef assert
+	//#define assert(...)
+#endif
+#define debug if(DEBUG)
+
+/* --------------------------------- MODULO --------------------------------- */
+struct mint
+{
+	static int mod;
+	int x;
+
+	inline int _modulize(int x) const
+	{
+		x %= mod;
+		if (x < 0) x += mod;
+		return x;
+	}
+	mint(int x = 0) : x(_modulize(x))
+	{
+	}
+	mint(LL x) : x(_modulize(x % mod))
+	{
+	}
+
+	operator int& ()
+	{
+		return x;
+	}
+
+	mint operator- () const
+	{
+		return mint(x == 0 ? 0: mod - x);
+	}
+
+	mint& operator+= (const mint& rhs)
+	{
+		x += rhs.x;
+		if (x >= mod) x -= mod;
+		return *this;
+	}
+	mint& operator+= (int y)
+	{
+		return *this += mint(y);
+	}
+	mint& operator+= (LL y)
+	{
+		return *this += mint(y);
+	}
+	mint operator+ (const mint& rhs) const
+	{
+		return mint(*this) += rhs;
+	}
+	mint operator+ (int y) const
+	{
+		return *this + mint(y);
+	}
+	mint operator+ (LL y) const
+	{
+		return *this + mint(y);
+	}
+	friend mint operator+ (int x, const mint& rhs)
+	{
+		return mint(x) + rhs;
+	}
+	friend mint operator+ (LL x, const mint& rhs)
+	{
+		return mint(x) + rhs;
+	}
+
+	mint& operator-= (const mint& rhs)
+	{
+		return *this += -rhs;
+	}
+	mint& operator-= (int y)
+	{
+		return *this -= mint(y);
+	}
+	mint& operator-= (LL y)
+	{
+		return *this -= mint(y);
+	}
+	mint operator- (const mint& rhs) const
+	{
+		return mint(*this) -= rhs;
+	}
+	mint operator- (int y) const
+	{
+		return *this - mint(y);
+	}
+	mint operator- (LL y) const
+	{
+		return *this - mint(y);
+	}
+	friend mint operator- (int x, const mint& rhs)
+	{
+		return mint(x) - rhs;
+	}
+	friend mint operator- (LL x, const mint& rhs)
+	{
+		return mint(x) - rhs;
+	}
+
+	mint& operator*= (const mint& rhs)
+	{
+		x = (LL)x * rhs.x % mod;
+		return *this;
+	}
+	mint& operator*= (int y)
+	{
+		return *this *= mint(y);
+	}
+	mint& operator*= (LL y)
+	{
+		return *this *= mint(y);
+	}
+	mint operator* (const mint& rhs) const
+	{
+		return mint(*this) *= rhs;
+	}
+	mint operator* (int y) const
+	{
+		return *this * mint(y);
+	}
+	mint operator* (LL y) const
+	{
+		return *this * mint(y);
+	}
+	friend mint operator* (int x, const mint& rhs)
+	{
+		return mint(x) * rhs;
+	}
+	friend mint operator* (LL x, const mint& rhs)
+	{
+		return mint(x) * rhs;
+	}
+
+	//wolniejsze
+	mint pow(int e) const
+	{
+		mint res = 1;
+		for (mint p = *this; e > 0; p *= p, e /= 2)
+		{
+			if (e % 2)
+				res *= p;
+		}
+		return res;
+	}
+	mint inv() const
+	{
+		return pow(mod - 2);
+	}
+	
+	mint& operator/= (const mint& rhs)
+	{
+		x *= rhs.inv();
+		return *this;
+	}
+	mint& operator/= (int y)
+	{
+		return *this /= mint(y);
+	}
+	mint& operator/= (LL y)
+	{
+		return *this /= mint(y);
+	}
+	mint operator/ (const mint& rhs) const
+	{
+		return mint(*this) /= rhs;
+	}
+	mint operator/ (int y) const
+	{
+		return *this / mint(y);
+	}
+	mint operator/ (LL y) const
+	{
+		return *this / mint(y);
+	}
+	friend mint operator/ (int x, const mint& rhs)
+	{
+		return mint(x) / rhs;
+	}
+	friend mint operator/ (LL x, const mint& rhs)
+	{
+		return mint(x) / rhs;
+	}
+};
+int mint::mod = 1'000'000'007;
+/* ------------------------------------------------------------------------ */
+
+struct Tree
+{
+	int LC = 4;
+	vector<int> tree;
+	Tree(int n)
+	{
+		while (LC < n) LC *= 2;
+		tree.resize(2 * LC, 0);
+	}
+	int less(int x)
+	{
+		x += LC;
+		int res = 0;
+		while (x > 1)
+		{
+			if (x % 2 == 1) 
+				res += tree[x - 1];
+			x /= 2;
+		}
+		return res;
+	}
+	void insert(int x)
+	{
+		tree[x += LC] = 1;
+		while (x /= 2) 
+			tree[x] = tree[2*x] + tree[2*x+1];
+	}
+};
+
+int main()
+{
+	SONIC_SPEED();
+
+	int n; cin >> n;
+	vector<int> p(n);
+	for (auto& pi : p) cin >> pi;
+
+	auto g = [](int t)->mint {return (t + 1) / 2; };
+	vector<mint> f(n + 1, 0); 
+	FOR(i, 1, n) 
+		f[i] = i * f[i-1]  +  g(i) * (i-1);
+
+	Tree T(n+1);
+	mint res = 0;
+	DFOR(i, n - 1, 0)
+	{
+		T.insert(p[i]);
+		int s = T.less(p[i]);
+
+		int t = n - i;
+		res += s * (g(t) + f[t-1]);
+	}
+
+	cout << res << "\n";
+}
